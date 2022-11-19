bf7772a1d0e3cf403b5887b9a9b67e7bee17f8d0
ElbaNrossSS <98917123+ElbaNrossSS@users.noreply.github.com>
2022-11-19 22:37:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js")
 const keep_alive = require('./keep_alive.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
 const ayarlar = require("./ayarlar.json");
-const { APIUser } = require('discord-api-types/v10');
 const db = require("nrc.db");
 const message = require("./events/message");
 require('dotenv').config();
